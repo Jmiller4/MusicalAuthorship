af05bd43e0d2941ce8ff9b4569773f86f9424d99
Rachel Katz <rkatz235@gmail.com>
2016-12-05 15:01:23 -0500

    returns chordList as romanNum & bass as Intervals

--- convert.py
+++ convert.py
@@ -1,18 +1,21 @@
 from music21 import *
 
 def main():
-	#get list of pieces
-	piece = corpus.parse("bwv66.6")
-	key = piece.analyze('key')
-	chords = piece.chordify()
-	
-	timeList = []
-	print(len(piece))
-	#romanizing the chords
-	for chord in chords.recurse().getElementsByClass('Chord'):
-		print(chord.measureNumber, chord.beatStr, roman.romanNumeralFromChord(chord, key))
-	#figured bass line
-	harmonies = figuredBass.checker.extractHarmonies(piece)
-	for (offsets, notes) in sorted(harmonies.items()):
-		print("{0!s:15}[{1!s:23}{2!s:23}{3!s:22}]".format(offsets, notes[0], notes[1], notes[2]))
-main() 
+        #get list of pieces
+        piece = corpus.parse("bwv66.6")
+        key = piece.analyze('key')
+        chords = piece.chordify()
+        chordList = []
+        bassLine = []
+        #romanizing the chords
+        for chord in chords.recurse().getElementsByClass('Chord'):
+                chordList.append(roman.romanNumeralFromChord(chord, key).figure)
+        #figured bass line
+        harmonies = figuredBass.checker.extractHarmonies(piece)
+        for (offsets, notes) in sorted(harmonies.items()):
+                bassLine.append([interval.Interval(note.Note(key.tonic), notes[0]).chromatic.semitones, interval.Interval(note.Note(key.tonic), notes[1]).chromatic.semitones, interval.Interval(note.Note(key.tonic),notes[2]).chromatic.semitones])
+        print(chordList)
+        print(bassLine)
+        return chordList, bassLine
+main()
+
