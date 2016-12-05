3313fc2a71b4eb3b5dfe672d6a12b15e55873594
Rachel Katz <rkatz235@gmail.com>
2016-12-05 15:11:53 -0500

    changed to class

--- convert.py
+++ convert.py
@@ -1,21 +1,26 @@
 from music21 import *
 
-def main():
-        #get list of pieces
-        piece = corpus.parse("bwv66.6")
-        key = piece.analyze('key')
-        chords = piece.chordify()
-        chordList = []
-        bassLine = []
-        #romanizing the chords
-        for chord in chords.recurse().getElementsByClass('Chord'):
-                chordList.append(roman.romanNumeralFromChord(chord, key).figure)
-        #figured bass line
-        harmonies = figuredBass.checker.extractHarmonies(piece)
-        for (offsets, notes) in sorted(harmonies.items()):
-                bassLine.append([interval.Interval(note.Note(key.tonic), notes[0]).chromatic.semitones, interval.Interval(note.Note(key.tonic), notes[1]).chromatic.semitones, interval.Interval(note.Note(key.tonic),notes[2]).chromatic.semitones])
-        print(chordList)
-        print(bassLine)
-        return chordList, bassLine
-main()
+class Convert:
+
+	def __init__(self, file):
+		self.file = file
+		self.chords = []
+		self.bass = []
+	def convert(self):
+		#get list of pieces
+		piece = corpus.parse(self.file)
+		key = piece.analyze('key')
+		chords = piece.chordify()
+		#romanizing the chords
+		for chord in chords.recurse().getElementsByClass('Chord'):
+			self.chords.append(roman.romanNumeralFromChord(chord, key).figure)
+		#figured bass line
+		harmonies = figuredBass.checker.extractHarmonies(piece)
+		for (offsets, notes) in sorted(harmonies.items()):
+			self.bass.append([interval.Interval(note.Note(key.tonic), notes[0]).chromatic.semitones, interval.Interval(note.Note(key.tonic), notes[1]).chromatic.semitones, interval.Interval(note.Note(key.tonic),notes[2]).chromatic.semitones])
+	def getBass(self):
+		return self.bass
+	def getChords(self):
+		return self.chords	
+
 
