06a9bb40e1e549e46c04b187dc9db1b56516c5b1
Jmiller4 <jmiller4@oberlin.edu>
2016-12-15 00:03:50 -0500

    changed main2 a little

--- main.py
+++ main.py
@@ -166,9 +166,38 @@ def main2():
 			for n_toUse in range(n+1)[1:]:
 				resultsDict[backoff][split][n_toUse] = testWithParameters(testing, learners, n_toUse, backoff, split)
 
-	print(resultsDict)
+	tableTop = "\\begin{center}\n    \\begin{tabular}{| l | l | l | l | l | l |}\n    \\hline\n"
+	tableTop += " & 0.0 & 0.25 & 0.5 & 0.75 & 1.0 \\\\ \\hline\n"
 
+	table1 = tableTop
+	for n_used in range(n+1)[1:]:
+		table1 += "   " + str(n_used)
+		for split in [0.0, 0.25, 0.5, 0.75, 1.0]:
+			acc = resultsDict[True][split][n_used][3]
+			acc_s = "%.4f"%(acc)
+			table1 += " & " + acc_s
+		table1 += " \\\\ \\hline\n"
 
+	table1 += "    \\end{tabular}\nWith Backoff\n\\end{center}\n"
+
+	table2 = tableTop 
+
+	for n_used in range(n+1)[1:]:
+		table2 += "   " + str(n_used)
+		for split in [0.0, 0.25, 0.5, 0.75, 1.0]:
+			acc = resultsDict[False][split][n_used][3]
+			acc_s = "%.4f"%(acc)
+			table2 += " & " + acc_s
+		table2 += " \\\\ \\hline\n"
+
+	table2 += "    \\end{tabular}\nWithout Backoff\n\\end{center}\n"
+
+	fileObject = open("RESULTS.txt", 'w')
+	fileObject.write(table1)
+	fileObject.write(table2)
+	fileObject.write(str(resultsDict))
+	fileObject.close()
+	print("wrote to file: RESULTS. see ya.")
 
 def main():
 	toPredict = ""
@@ -208,7 +237,7 @@ def main():
 			print("wrote to file. see ya.")
 
 
-main()
+main2()
 
 #TODO: experiment with a main that after making a prediction, adds the predicted piece to that n-gram's training data; 
 #sort of a semi evolutionary algorithm
